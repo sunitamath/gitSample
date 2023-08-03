6463653a247a79a02c89d0e50e24e1be86753096
sunitamath <sunita13.math2@gmail.com>
2023-08-03 20:47:02 +0100

    Demo is committed

---
+++ src/test/java/Sample/demo.java
@@ -0,0 +1,16 @@
+package Sample;
+
+import org.testng.annotations.Test;
+
+public class demo {
+
+	@Test
+	public void sample()
+	{
+		System.out.println("Hi this is my first commit");
+	}
+	
+		
+}
+
+
